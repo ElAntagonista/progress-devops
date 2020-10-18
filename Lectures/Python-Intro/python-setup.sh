6418fd85dbcd56242eba7a687ea6971f73556966
ElAntagonista <kris.membe@gmail.com>
2020-10-18 19:43:42 +0300

    Fix shebang in python-setup.sh

--- Lectures/Python-Intro/python-setup.sh
+++ Lectures/Python-Intro/python-setup.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
 sudo apt update && sudo apt install -y python3.8 python3-pip python3.8-venv
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 40
 echo "alias python=python3" >> ~/.bashrc
 echo "alias pip=pip3" >> ~/.bashrc
-
