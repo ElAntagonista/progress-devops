4084d4a4af3fefe2dc46347a5f7914daaaf9865b
ElAntagonista <kris.membe@gmail.com>
2020-10-20 19:18:58 +0300

    Add bash intro homework

--- Lectures/Bash-Intro/Homework/task-1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-packages=(python3-pip python3)
-for package in ${packages[@]}
-do
- dpkg-query --show $package &> /dev/null && \
- echo "${package} is installed" || \
- echo "${package} is nowhere to be found" 
-done
-
