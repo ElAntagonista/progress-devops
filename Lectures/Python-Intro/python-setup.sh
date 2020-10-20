e38c5be99b21561e15336f1bc467253a3ff4e597
ElAntagonista <kris.membe@gmail.com>
2020-10-20 17:42:50 +0300

    Fix python-setup

--- Lectures/Python-Intro/python-setup.sh
+++ Lectures/Python-Intro/python-setup.sh
@@ -1,3 +1,4 @@
+#! /usr/bin/env bash
 sudo apt update && sudo apt install -y python3.8 python3-pip python3.8-venv
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 40
 echo "alias python=python3" >> ~/.bashrc
