0b80cf94dc3465257fd049850fc2f37d43b359f7
ElAntagonista <kris.membe@gmail.com>
2020-11-08 18:25:44 +0200

    Update setup.sh to install awscli only

--- Lectures/AWS/setup.sh
+++ Lectures/AWS/setup.sh
@@ -1,7 +1,2 @@
 #! /usr/bin/env bash
-sudo apt update && sudo apt install -y python3.8 python3-pip awscli
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 40
-echo "alias python=python3" >> ~/.bashrc
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+sudo apt update && sudo apt install -y awscli
